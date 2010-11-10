36f6d213398b6ac0bc39d39a3f6993b2c9fa1716
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-10 16:16:31 +0000

    Auto triple digits on weeks and days, new style selector and new css style called carbonite
    git-svn-id: https://plugins.svn.wordpress.org/jquery-t-countdown-widget/trunk@310205 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.lwtCountdown-1.0.js
+++ js/jquery.lwtCountdown-1.0.js
@@ -1,15 +1,17 @@
 /*!
  * jQuery Countdown plugin v1.1
+ * http://www.twinpictures.de/t-countdown-widget
  * http://www.littlewebthings.com/projects/countdown/
  *
- * Copyright 2010, Vassilis Dourdounis, Twinpictures
+ * Copyright 2010, Twinpictures, Vassilis Dourdounis
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * to use, copy, modify, merge, publish, distribute, sublicense, blend, trade,
+ * bake, hack, scramble, difiburlate, digest and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
@@ -22,6 +24,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 (function($){
 
 	$.fn.countDown = function (options) {
@@ -31,12 +34,10 @@
 
 		diffSecs = this.setCountDown(config);
 	
-		if (config.onComplete)
-		{
+		if (config.onComplete){
 			$.data($(this)[0], 'callback', config.onComplete);
 		}
-		if (config.omitWeeks)
-		{
+		if (config.omitWeeks){
 			$.data($(this)[0], 'omitWeeks', config.omitWeeks);
 		}
 
@@ -58,12 +59,10 @@
 	$.fn.setCountDown = function (options) {
 		var targetTime = new Date();
 
-		if (options.targetDate)
-		{
+		if (options.targetDate){
 			targetTime = new Date(options.targetDate.month + '/' + options.targetDate.day + '/' + options.targetDate.year + ' ' + options.targetDate.hour + ':' + options.targetDate.min + ':' + options.targetDate.sec + (options.targetDate.utc ? ' UTC' : ''));
 		}
-		else if (options.targetOffset)
-		{
+		else if (options.targetOffset){
 			targetTime.setFullYear(options.targetOffset.year + targetTime.getFullYear());
 			targetTime.setMonth(options.targetOffset.month + targetTime.getMonth());
 			targetTime.setDate(options.targetOffset.day + targetTime.getDate());
@@ -84,11 +83,9 @@
 
 	$.fn.doCountDown = function (id, diffSecs, duration) {
 		$this = $('#' + id);
-		if (diffSecs <= 0)
-		{
+		if (diffSecs <= 0){
 			diffSecs = 0;
-			if ($.data($this[0], 'timer'))
-			{
+			if ($.data($this[0], 'timer')){
 				clearTimeout($.data($this[0], 'timer'));
 			}
 		}
@@ -96,13 +93,12 @@
 		secs = diffSecs % 60;
 		mins = Math.floor(diffSecs/60)%60;
 		hours = Math.floor(diffSecs/60/60)%24;
-		if ($.data($this[0], 'omitWeeks') == true)
-		{
+		
+		if ($.data($this[0], 'omitWeeks') == true){
 			days = Math.floor(diffSecs/60/60/24);
 			weeks = Math.floor(diffSecs/60/60/24/7);
 		}
-		else 
-		{
+		else{
 			days = Math.floor(diffSecs/60/60/24)%7;
 			weeks = Math.floor(diffSecs/60/60/24/7);
 		}
@@ -111,41 +107,38 @@
 		$this.dashChangeTo(id, 'minutes_dash', mins, duration ? duration : 1000);
 		$this.dashChangeTo(id, 'hours_dash', hours, duration ? duration : 1000);
 		$this.dashChangeTo(id, 'days_dash', days, duration ? duration : 1000);
+		$this.dashChangeTo(id, 'days_trip_dash', days, duration ? duration : 1000);
 		$this.dashChangeTo(id, 'weeks_dash', weeks, duration ? duration : 1000);
+		$this.dashChangeTo(id, 'weeks_trip_dash', weeks, duration ? duration : 1000);
 
 		$.data($this[0], 'diffSecs', diffSecs);
-		if (diffSecs > 0)
-		{
+		if (diffSecs > 0){
 			e = $this;
 			t = setTimeout(function() { e.doCountDown(id, diffSecs-1) } , 1000);
 			$.data(e[0], 'timer', t);
 		} 
-		else if (cb = $.data($this[0], 'callback')) 
-		{
+		else if (cb = $.data($this[0], 'callback')){
 			$.data($this[0], 'callback')();
 		}
 
 	};
 
+        
 	$.fn.dashChangeTo = function(id, dash, n, duration) {
-		  $this = $('#' + id);
-		 
-		  for (var i=($this.find('.' + dash + ' .digit').length-1); i>=0; i--)
-		  {
-				var d = n%10;
-				n = (n - d) / 10;
-				$this.digitChangeTo('#' + $this.attr('id') + ' .' + dash + ' .digit:eq('+i+')', d, duration);
-		  }
+		$this = $('#' + id);
+		//loop through each digit and chage that dude
+		for (var i=($this.find('.' + dash + ' .digit').length-1); i>=0; i--){
+			var d = n%10;
+			n = (n - d) / 10;
+			$this.digitChangeTo('#' + $this.attr('id') + ' .' + dash + ' .digit:eq('+i+')', d, duration);
+		}
 	};
-
+	
 	$.fn.digitChangeTo = function (digit, n, duration) {
-		if (!duration)
-		{
+		if (!duration){
 			duration = 500;
 		}
-		if ($(digit + ' div.top').html() != n + '')
-		{
-
+		if ($(digit + ' div.top').html() != n + ''){
 			$(digit + ' div.top').css({'display': 'none'});
 			// $(digit + ' div.bottom').css({'border-top': '1px solid #666'});
 			$(digit + ' div.top').html((n ? n : '0')).slideDown(duration);
@@ -155,8 +148,6 @@
 				$(digit + ' div.bottom').css({'display': 'block', 'height': ''});
 				// $(digit + ' div.bottom').css({'border-top': 'none'});
 				$(digit + ' div.top').hide().slideUp(10);
-
-			
 			});
 		}
 	};
